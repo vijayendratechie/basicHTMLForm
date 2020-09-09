7b78a7e48836944e577dab849c056984cbd821fb
vijayendra <vijayendra@automate.io>
2020-09-09 18:40:45 +0530

    Created basic form

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const express = require("express");
-const request = require("request");
 const fs = require('fs');
-const cheerio = require('cheerio');
 
 const app = express();
 
 app.get("/",function(req,res){
-	res.send('<html><p>Please click the link <a href="https://latest5news.herokuapp.com/latestNews">https://latest5news.herokuapp.com/latestNews</a> to display latest 5 news from times.com</p></html>')
+	res.sendFile('basicForm.html', {root: __dirname })
 })
 
-app.get("/latestNews",function(req,res){
-	request({uri: "https://time.com/"},function(error, response, body) {
-		if(error) {
-			res.send("Error ocurred. Please try after sometime");
-		}
-
-		let $ = cheerio.load(body);
-
-		let latestStoriesSection = $("section[data-module_name='Latest Stories']");
-		latestStoriesSection = latestStoriesSection.html();
-		$ = cheerio.load(latestStoriesSection)
-
-		let latest5News = [];
-
-		$("li").each(function(i) {
-			latest5News.push({
-				"title": $(this).find("a").text(),	
-				"link": "https://time.com/"+$(this).find("a").attr("href")
-			})
-		})
-
-		res.send(latest5News);
-	  });
-
+app.post("/",function(req,res){
+	res.send("Form Submitted successfully");
 })
 
 const port=process.env.PORT || 3000
